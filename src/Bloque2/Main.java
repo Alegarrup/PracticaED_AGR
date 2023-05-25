b6f62d806a4e57f481d499306e3e037639a8f350
usuario <angelromerocarmona2004@gmail.com>
2023-05-25 14:05:52 +0200

    El main fue creado y se probaron los dos métodos

---
+++ src/Bloque2/Main.java
@@ -0,0 +1,9 @@
+package Bloque2;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	System.out.println(Ejercicio1_Bloque2.calcularFactorial(4));
+	System.out.println(Ejercicio2_Bloque2.CalcularPrimo(6));
+	}
+}
