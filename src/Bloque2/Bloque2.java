e1637ebb8c537e86c524361e474ca867a8967df8
usuario <alegarrup9@gmail.com>
2023-05-25 13:49:55 +0200

    Cambiado la clase, añadido javadoc

--- src/Bloque2/Bloque2.java
+++ src/Bloque2/Bloque2.java
@@ -10,15 +10,26 @@ public class Bloque2 {
 	}
 }
 
+/***
+ * 
+ * 
+ * @author Alegarrup
+ *
+ *Hay que calcular el factorial de un número
+ *Se elige un numero para calcular
+ */
+
 class Ejercicio1_Bloque2 {
-	// Hay que calcular el factorial de un número
-	// Se elige un numero para calcular
+	
 	public static int calcularFactorial(int eleccion_numero) {
-		// creamos variable factorial que almacene el resultado
+		/** creamos variable factorial que almacene el resultado
+		 * 
+		 */
 		int factorial = 1;
 
-		// Se hace un while que cada vez que itere el numero sea -1 y resuelva el
-		// factorial
+		 /**Se hace un while que cada vez que itere el numero sea -1 y resuelva el
+		 factorial
+		 */
 		while (eleccion_numero != 0) {
 			factorial = factorial * eleccion_numero;
 			eleccion_numero--;
@@ -29,22 +40,27 @@ class Ejercicio1_Bloque2 {
 
 }
 
-//Segundo ejercicio
+/**Segundo ejercicio*/
 class Ejercicio2_Bloque2 {
-	// Calcular si un numero es primo o no
-
-	// Creamos el método y Se elige el numero a calcular
+	 /**Calcular si un numero es primo o no
+     *
+	Creamos el método y Se elige el numero a calcular
+	*/
 	public static boolean CalcularPrimo(int eleccion_numero) {
 
 		for (int i = 2; i < eleccion_numero / 2; i++) {
-			// Si es divisible por cualquiera de estos números, no
-			// es primo
+			 /**
+			  * Si es divisible por cualquiera de estos números, no
+			  *es primo
+			  */
 			if (eleccion_numero % i == 0)
 				return false;
 
 		}
 
-		// Si no es divisible, devuelve true
+		/** Si no es divisible, devuelve true
+		 * el metodo
+		 */
 		return true;
 
 	}
